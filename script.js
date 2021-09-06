6ef516ff989e139a0105b1ad1e24c6a8c627125d
adylbek <aaaadyl.erg@gmail.com>
2021-09-06 13:21:34 +0600

    .

---
+++ script.js
@@ -0,0 +1,26 @@
+const animEls = document.querySelectorAll('img');
+const cherries = document.querySelector('.cherries');
+
+
+animEls.forEach(img => {
+  //random number between 0 to 90
+  const randomNumLeft = Math.floor(Math.random() * (90 + 1));
+  //random negative number between 0 to 40
+  const randomNumTop = Math.floor(Math.random() * (2000 + 1)) * -1;
+
+  //setting random num to the falling img left value
+  img.style.left = `${randomNumLeft}%`;
+
+  //setting negative random num as attribute "top" to the falling img
+  img.setAttribute('top', `${randomNumTop}`);
+  img.style.top = `${randomNumTop}px`;
+});
+
+window.addEventListener('scroll', () => {
+  animEls.forEach(img => {
+    const imgTop = img.getAttribute('top')
+    const scrollValue = window.scrollY;
+
+    img.style.top = `${Number(scrollValue) * 2.2 + (Number(imgTop))}px`;
+  });
+})
